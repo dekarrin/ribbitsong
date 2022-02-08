cb849ad38b8346045c4c5f6ea5aaf8f20c7ad02f
Becky Nelson <dekarrin.irl@gmail.com>
2022-02-08 10:12:48 -0600

    data entry now uses last default

--- pysrc/ribbitsong/ribbitsong.py
+++ pysrc/ribbitsong/ribbitsong.py
@@ -6,6 +6,7 @@ from typing import Tuple, Optional
 
 import json
 
+# TODO: this module should be called cherub
 
 def show_data_menu():
     unsaved_mutations = False
@@ -323,83 +324,83 @@ def create_event_tag_field(parent_form, field_name, multivalue=False, nullable=F
     tag_forms = parent_form.add_polymorphic_object_field(field_name, types, multivalue=multivalue, nullable=nullable)
     
     appearance_changed_form = tag_forms[0]
-    appearance_changed_form.add_field("recipient")
-    appearance_changed_form.add_field("appearance")
+    appearance_changed_form.add_field("recipient", default_last=True)
+    appearance_changed_form.add_field("appearance", default_last=True)
     
     state_changed_form = tag_forms[1]
-    state_changed_form.add_field("recipient")
-    state_changed_form.add_field("property")
-    state_changed_form.add_field("value")
+    state_changed_form.add_field("recipient", default_last=True)
+    state_changed_form.add_field("property", default_last=True)
+    state_changed_form.add_field("value", default_last=True)
     
     char_obtains_item_form = tag_forms[2]
-    char_obtains_item_form.add_field("character")
-    char_obtains_item_form.add_field("item")
+    char_obtains_item_form.add_field("character", default_last=True)
+    char_obtains_item_form.add_field("item", default_last=True)
     
     char_drops_item_form = tag_forms[3]
-    char_drops_item_form.add_field("character")
-    char_drops_item_form.add_field("item")
+    char_drops_item_form.add_field("character", default_last=True)
+    char_drops_item_form.add_field("item", default_last=True)
     
     char_uses_item_form = tag_forms[4]
-    char_uses_item_form.add_field("character")
-    char_uses_item_form.add_field("item")
-    char_uses_item_form.add_field("consumed", type=bool)
+    char_uses_item_form.add_field("character", default_last=True)
+    char_uses_item_form.add_field("item", default_last=True)
+    char_uses_item_form.add_field("consumed", type=bool, default_last=True)
     
     char_gives_item_form = tag_forms[5]
-    char_gives_item_form.add_field("giver")
-    char_gives_item_form.add_field("receiver")
-    char_gives_item_form.add_field("item")
+    char_gives_item_form.add_field("giver", default_last=True)
+    char_gives_item_form.add_field("receiver", default_last=True)
+    char_gives_item_form.add_field("item", default_last=True)
     
     char_dies_form = tag_forms[6]
-    char_dies_form.add_field("character")
+    char_dies_form.add_field("character", default_last=True)
     
     char_born_form = tag_forms[7]
-    char_born_form.add_field("character")
+    char_born_form.add_field("character", default_last=True)
     
     char_resurrected_form = tag_forms[8]
-    char_resurrected_form.add_field("character")
+    char_resurrected_form.add_field("character", default_last=True)
     
     char_ports_in_form = tag_forms[9]
-    char_ports_in_form.add_field("character")
-    char_ports_in_form.add_field("port_out_event", nullable=True)
+    char_ports_in_form.add_field("character", default_last=True)
+    char_ports_in_form.add_field("port_out_event", nullable=True, default_last=True)
     
     char_ports_out_form = tag_forms[10]
-    char_ports_out_form.add_field("character")
-    char_ports_out_form.add_field("port_in_event", nullable=True)
+    char_ports_out_form.add_field("character", default_last=True)
+    char_ports_out_form.add_field("port_in_event", nullable=True, default_last=True)
     
     char_enters_form = tag_forms[11]
-    char_enters_form.add_field("character")
+    char_enters_form.add_field("character", default_last=True)
     
     char_exits_form = tag_forms[12]
-    char_exits_form.add_field("character")
+    char_exits_form.add_field("character", default_last=True)
     
     char_falls_asleep_form = tag_forms[13]
-    char_falls_asleep_form.add_field("character")
+    char_falls_asleep_form.add_field("character", default_last=True)
     
     char_wakes_up_form = tag_forms[14]
-    char_wakes_up_form.add_field("character")
+    char_wakes_up_form.add_field("character", default_last=True)
     
     
 def create_citation_field(parent_form, field_name, multivalue=False, nullable=False):
     types = ["dialog", "narration", "media", "commentary"]
     citation_forms = parent_form.add_polymorphic_object_field(field_name, types, multivalue=multivalue, nullable=nullable)
     cite_dialog_form, cite_narration_form, cite_media_form, cite_commentary_form = citation_forms
-    cite_dialog_form.add_field("work")
-    cite_dialog_form.add_field("panel", type=int)
-    cite_dialog_form.add_field("line", type=int)
-    cite_dialog_form.add_field("character")
+    cite_dialog_form.add_field("work", default_last=True)
+    cite_dialog_form.add_field("panel", type=int, default_last=True)
+    cite_dialog_form.add_field("line", type=int, default_last=True)
+    cite_dialog_form.add_field("character", default_last=True)
     
-    cite_narration_form.add_field("work")
-    cite_narration_form.add_field("panel", type=int)
-    cite_narration_form.add_field("paragraph", type=int)
-    cite_narration_form.add_field("sentence", type=int)
+    cite_narration_form.add_field("work", default_last=True)
+    cite_narration_form.add_field("panel", type=int, default_last=True)
+    cite_narration_form.add_field("paragraph", type=int, default_last=True)
+    cite_narration_form.add_field("sentence", type=int, default_last=True)
     
-    cite_media_form.add_field("work")
-    cite_media_form.add_field("panel", type=int)
-    cite_media_form.add_field("timestamp")
+    cite_media_form.add_field("work", default_last=True)
+    cite_media_form.add_field("panel", type=int, default_last=True)
+    cite_media_form.add_field("timestamp", default_last=True)
     
-    cite_commentary_form.add_field("work")
-    cite_commentary_form.add_field("volume", type=int)
-    cite_commentary_form.add_field("page", type=int)
+    cite_commentary_form.add_field("work", default_last=True)
+    cite_commentary_form.add_field("volume", type=int, default_last=True)
+    cite_commentary_form.add_field("page", type=int, default_last=True)
     
 
 def create_constraint_field(parent_form, field_name, multivalue=False, nullable=False):
@@ -417,37 +418,37 @@ def create_constraint_field(parent_form, field_name, multivalue=False, nullable=
     constraint_forms = parent_form.add_polymorphic_object_field(field_name, types, multivalue=multivalue, nullable=nullable)
     
     immediate_form = constraint_forms[0]
-    immediate_form.add_field("ref_event")
-    immediate_form.add_field("is_after", type=bool, default=True)
+    immediate_form.add_field("ref_event", default_last=True)
+    immediate_form.add_field("is_after", type=bool, default=True, default_last=True)
     
     jump_form = constraint_forms[1]
-    jump_form.add_field("ref_event")
-    jump_form.add_field("is_after", type=bool, default=True)
+    jump_form.add_field("ref_event", default_last=True)
+    jump_form.add_field("is_after", type=bool, default=True, default_last=True)
     
     entrypoint_form = constraint_forms[2]
-    # there is no additional data
+    # there is no additional data for narrative entrypoint
     
     narrative_causal_form = constraint_forms[3]
-    narrative_causal_form.add_field("ref_event")
-    narrative_causal_form.add_field("is_after", type=bool, default=True)
+    narrative_causal_form.add_field("ref_event", default_last=True)
+    narrative_causal_form.add_field("is_after", type=bool, default=True, default_last=True)
     
     abs_form = constraint_forms[4]
-    abs_form.add_field("time")
-    create_citation_field(abs_form, "citation", nullable=False)
+    abs_form.add_field("time", default_last=True)
+    create_citation_field(abs_form, "citation", nullable=False, default_last=True)
     
     relative_form = constraint_forms[5]
-    relative_form.add_field("ref_event")
-    relative_form.add_field("is_after", type=bool, default=True)
-    relative_form.add_field("distance")
-    create_citation_field(relative_form, "citation", nullable=False)
+    relative_form.add_field("ref_event", default_last=True)
+    relative_form.add_field("is_after", type=bool, default=True, default_last=True)
+    relative_form.add_field("distance", default_last=True)
+    create_citation_field(relative_form, "citation", nullable=False, default_last=True)
     
     causal_form = constraint_forms[6]
-    causal_form.add_field("time")
-    create_citation_field(causal_form, "citation", nullable=False)
+    causal_form.add_field("time", default_last=True)
+    create_citation_field(causal_form, "citation", nullable=False, default_last=True)
     
     sync_form = constraint_forms[7]
-    sync_form.add_field("ref_event")
-    create_citation_field(sync_form, "citation", nullable=False)
+    sync_form.add_field("ref_event", default_last=True)
+    create_citation_field(sync_form, "citation", nullable=False, default_last=True)
     
 def enter_data() -> list[dict]:
     """
@@ -459,19 +460,19 @@ def enter_data() -> list[dict]:
     event_form = form.add_object_field("events", multivalue=True)
     
     event_form.add_auto_uuid_field("id")
-    event_form.add_field("name")
-    event_form.add_field("description")
+    event_form.add_field("name", default_last=True)
+    event_form.add_field("description", default_last=True)
     create_citation_field(event_form, "citations", multivalue=True)
     create_citation_field(event_form, "portrayed_in", nullable=True)
     create_event_tag_field(event_form, "tags", multivalue=True)
     create_constraint_field(event_form, "constraints", multivalue=True)
     
     univ_form = event_form.add_object_field("universes", multivalue=True)
-    univ_form.add_field("name")
-    univ_form.add_field("timeline")
-    univ_form.add_field("location")
-    univ_form.add_field("characters", multivalue=True)
-    univ_form.add_field("items", multivalue=True)
+    univ_form.add_field("name", default_last=True)
+    univ_form.add_field("timeline", default_last=True)
+    univ_form.add_field("location", default_last=True)
+    univ_form.add_field("characters", multivalue=True, default_last=True)
+    univ_form.add_field("items", multivalue=True, default_last=True)
     
     data = form.fill()
     
